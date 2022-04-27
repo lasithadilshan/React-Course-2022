454736a97f9cfa85dd3e79890f4fc4a66c3f2bf3
Lasitha Thilakarathna <dilshantilakaratne29@gmail.com>
2022-04-27 16:42:54 +0530

    js file updated
    JavaScript  codes added

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
-ReactDOM.render(<p>Hi, my name is Bob!</p>, document.getElementById("root"))
+// ReactDOM.render(<p>Hi, my name is Bob!</p>, document.getElementById("root"))
+ReactDOM.render(
+    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+     document.getElementById("root"))
