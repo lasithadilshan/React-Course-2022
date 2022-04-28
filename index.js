4010a5b9fc7ab85ed6fe3162723a0c4bad4c674b
Lasitha Thilakarathna <dilshantilakaratne29@gmail.com>
2022-04-28 07:51:28 +0530

    js file updated
    add some js code

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
 // ReactDOM.render(<p>Hi, my name is Bob!</p>, document.getElementById("root"))
-ReactDOM.render(
-    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
-     document.getElementById("root"))
+// ReactDOM.render(
+//     <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+//      document.getElementById("root"))
+
+const h1 = document.createElement("h1")
+h1.textContent = "This is an imperative way to program"
+h1.className = "header"
+document.getElementById("root").append(h1)
