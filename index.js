2cefcb68db2a06911bd67441a4b4b97d481455d2
Lasitha Thilakarathna <dilshantilakaratne29@gmail.com>
2022-04-28 07:56:45 +0530

    js updated
    test some js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@
 //     <ul><li>Thing 1</li><li>Thing 2</li></ul>,
 //      document.getElementById("root"))
 
-const h1 = document.createElement("h1")
-h1.textContent = "This is an imperative way to program"
-h1.className = "header"
-document.getElementById("root").append(h1)
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is an imperative way to program"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
+
+ReactDOM.render(<h1 className="header">Hello, React!</h1>, document.getElementById("root"))
